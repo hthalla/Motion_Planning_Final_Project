216620c01bc7f9f17a519957e4b3f194a65abb01
sha-abhishek <asharm5@clemson.edu>
2023-04-18 17:42:26 -0400

    make grid function

--- hybrid_astar.py
+++ hybrid_astar.py
@@ -5,13 +5,21 @@ import car
 # input
 # obs: [(x_min,ymin,x_max,y_max),....]
 # grid_dim: [x_min,ymin,x_max,y_max]
+# grid_size: float
 
 # output: grid[[0,0,...1,1,0],[],[]] 2d array of size m*n
-def make_grid(obs,grid_dim):
-    pass
+def make_grid(grid_dim,cell_size):
+    lx = abs(grid_dim[0]-grid_dim[2])
+    ly = abs(grid_dim[1]-grid_dim[3])
+    nx = int(lx/cell_size)
+    ny = int(ly/cell_size)
+    grid = [[0 for i in range(nx)] for j in range(ny)]
+
+    return grid
 
 
 car = car.Car()
+
 
 
 # inputs
@@ -25,7 +33,19 @@ def hybrid_astar(grid,start_conf,goal_conf,):
     init_node = start_conf
     cur_node = init_node
 
-    next_confs = car.astar_step(cur_node)
+    next_confs = car.astar_step(cur_node)   # [(x,y,th)]
     
 
     pass
+
+def main():
+    grid_dimension = [0,0,10,10]
+    cell_size = 0.5
+    grid = make_grid(grid_dimension,cell_size)
+    print(grid)
+    print('grid shape:',len(grid),len(grid[0]))
+
+
+
+if __name__== main():
+    main()
