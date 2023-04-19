28c2b2ad6b43f32df9b68a789f9b9b682bbd74c6
sha-abhishek <asharm5@clemson.edu>
2023-04-18 20:57:21 -0400

    step

--- car.py
+++ car.py
@@ -1,14 +1,27 @@
 import numpy as np
-from math import pi
+from math import *
 
 class Car():
-    def __init__(self,l=0.3,w=0.2,max_phi=pi/5):
+    def __init__(self,l=1,w=0.2,max_phi=pi/6,v=1):
         self.l = l
         self.w = w
+        self.v = v
         self.max_phi = max_phi
         pass
 
-    def astar_step(cur_conf):
-        
-        next_confs = [(1,2,3),(2,3,4),(3,4,5)]  # [(x1,y1,th1),(),()]
-        pass
+    def astar_step(self,cur_conf):
+        x = cur_conf[0]
+        y = cur_conf[1]
+        th = cur_conf[2]
+
+        next_confs = []
+        actions = [-self.max_phi,0,self.max_phi]
+        dt = 1
+
+        for i in range(len(actions)):
+            xn = x + self.v*cos(actions[i])*dt
+            yn = y + self.v*sin(actions[i])*dt
+            thn = th + (self.v/self.l)*tan(actions[i])*dt
+            next_confs.append([xn,yn,thn])
+
+        return next_confs
