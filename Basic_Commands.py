58cf898acaf0bdca21fbeec0226461fe1f1e4f5f
hthalla <98755521+hthalla@users.noreply.github.com>
2023-04-29 15:37:18 -0400

    Defining env

--- Basic_Commands.py
+++ Basic_Commands.py
@@ -5,6 +5,11 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 env = gym.make("parking-parked-v0")
 done = truncated = False
+env.configure({
+    "screen_height": 600,
+    "screen_width": 1200
+})
+
 env.reset(seed=10)  # Ensuring we are getting the same scenario
 
 # The below command is only adding a max of 7 vehicles. Need to add manually.
@@ -25,12 +30,16 @@ v = env.vehicle.make_on_lane(env.road, lane, 4, speed=0)
 env.road.vehicles.append(v)
 
 # Ego vehicle position can be changed as below
-env.road.vehicles[0].heading = 0.0
-env.road.vehicles[0].position = np.array([5.0, 5.0])
+env.road.vehicles[0].heading = np.pi/2
+env.road.vehicles[0].position = np.array([-30.5, -17.5])
 
 # Parked vehicles can be changed as below
-env.road.vehicles[1].heading = 0.0
-env.road.vehicles[1].position = np.array([-2.0, -2.0])
+env.road.vehicles[1].heading = np.pi/2
+env.road.vehicles[1].position = np.array([2.0, 2.0])
+
+# Setting the goal position
+env.goal.heading = np.pi/2
+env.goal.position = np.array([14.0, 14.0])
 
 # Controll actions
 action = [0.1, 0.1]  # acceleration, steering
